25911217428d99a261c80f99f097d4894d709107
Yitao Ma - i323251 <i323251@I323251-LIN-002.pal.sap.corp>
2019-12-01 23:00:43 -0800

    added how to use makefile

---
+++ makefile/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "test.h"
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	cout << "starting from main..." << endl;
+	test();
+	return 0;
+}
+
---
+++ makefile/test.cpp
@@ -0,0 +1,9 @@
+#include "test.h"
+#include <iostream>
+
+using namespace std;
+
+void test() {
+	cout <<"this is test routine..."<<endl;
+}
+
